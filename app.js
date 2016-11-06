bee04ea33c1c65ad108725f0d42a2b20ddb3e939
MrCsabaToth <csaba.toth.us@gmail.com>
2016-11-05 22:20:31 -0700

    atoth works with http

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.get('/process', function(req, res) {
   var sessionsJson = JSON.parse(sessionsText);
   var speakersJson = JSON.parse(speakersText);
 
-  var linkStub = 'https://atoth.sote.hu/~tocsa/vdf2016r/?uuid=' 
+  var linkStub = 'http://atoth.sote.hu/~tocsa/vdf2016r/?uuid=' 
   var links = {};
   var categories = ['content', 'presentation', 'venue'];
   Object.keys(aggregated).forEach(function(agg) {
