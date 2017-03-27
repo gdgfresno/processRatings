5100ef0e36f70bc9a214b5f13db5300e98234490
MrCsabaToth <csaba.toth.us@gmail.com>
2017-03-26 22:26:04 -0700

    Processing other structured json

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/process', function(req, res) {
+  var organizerSpeaker = 14;
   var fs = require('fs');
   var ratingsText = fs.readFileSync('ratings.json', 'utf8');
   var ratingsJson = JSON.parse(ratingsText);
-  var conferenceStart = new Date('2016-10-22 8:00').getTime();
 
   var sessionsText = fs.readFileSync('sessions.json', 'utf8');
   var speakersText = fs.readFileSync('speakers.json', 'utf8');
@@ -32,7 +32,7 @@ app.get('/process', function(req, res) {
   var whole = [];
   var aggregateOutput = {
     categoryNames: categoryNames,
-    ratingTitles: ['Very dissatisfied', 'Dissatisfied', 'Neutral', 'Satisfied', 'Very satisfied']
+    ratingTitles: ['1', '2', '3', '4', '5']
   };
   categoryNames.forEach(function(cat) {
     categoryAggregate[cat] = {
@@ -48,38 +48,44 @@ app.get('/process', function(req, res) {
   });
 
   var aggregatedSessionData = {};
-  Object.keys(ratingsJson.ratings).forEach(function(key) {
-    var rating = ratingsJson.ratings[key];
-    if (rating.time > conferenceStart) {
-      if (Object.keys(aggregatedSessionData).indexOf(rating.session.toString()) < 0) {
-        aggregatedSessionData[rating.session] = {
-          rating: {
-            content: { values: [], distribution: [0, 0, 0, 0, 0] },
-            presentation: { values: [], distribution: [0, 0, 0, 0, 0] },
-            venue: { values: [], distribution: [0, 0, 0, 0, 0] }
-          },
-          comments: []
-        };
-      }
-      if (rating.rating) {
-        var sessionAggr = aggregatedSessionData[rating.session].rating[rating.category];
-        sessionAggr.values.push(rating.rating);
-        sessionAggr.distribution[rating.rating - 1]++;
-        var session = sessionsJson[rating.session];
-        if (!session.speakers || session.speakers.length <= 0) {
-          session.speakers = [14];  // Rio is also for Generic Sessions
+  Object.keys(ratingsJson).forEach(function(key) {
+    var rating = ratingsJson[key];
+    if (rating.sessions) {
+      Object.keys(rating.sessions).forEach(function(sessionId) {
+        var session = rating.sessions[sessionId];
+        if (Object.keys(aggregatedSessionData).indexOf(session.toString()) < 0) {
+          aggregatedSessionData[sessionId] = {
+            rating: {
+              content: { values: [], distribution: [0, 0, 0, 0, 0] },
+              presentation: { values: [], distribution: [0, 0, 0, 0, 0] },
+              venue: { values: [], distribution: [0, 0, 0, 0, 0] }
+            },
+            comments: []
+          };
         }
-        if (session.speakers.length > 1 || session.speakers[0] != 14) {
-          categoryAggregate[rating.category].values.push(rating.rating);
-          aggregateOutput[rating.category].distribution[rating.rating - 1]++;
+        categoryNames.forEach(function(cat) {
+          if (session[cat]) {
+            var sessionAggr = aggregatedSessionData[sessionId].rating[cat];
+            sessionAggr.values.push(session[cat]);
+            sessionAggr.distribution[session[cat] - 1]++;
+            var sess = sessionsJson[sessionId];
+            if (!sess.speakers || sess.speakers.length <= 0) {
+              sess.speakers = [organizerSpeaker];  // Container speaker for generic sessions
+            }
+            if (sess.speakers.length > 1 || sess.speakers[0] != organizerSpeaker) {
+              categoryAggregate[cat].values.push(session[cat]);
+              aggregateOutput[cat].distribution[session[cat] - 1]++;
+            }
+          }
+        });
+        if (session.comment) {
+          aggregatedSessionData[sessionId].comments.push(session.comment);
         }
-      } else {
-        aggregatedSessionData[rating.session].comments.push(rating.comment);
-      }
+      });
     }
   });
 
-  var linkStub = 'https://vdf2016r.firebaseapp.com/?uuid=' 
+  var linkStub = 'https://wtm2017r.firebaseapp.com/?uuid=' 
   var links = {};
   Object.keys(aggregatedSessionData).forEach(function(key) {
     var guid = uuid.v4().replace(/-/g, '');
@@ -142,13 +148,20 @@ app.get('/process', function(req, res) {
 
   categoryNames.forEach(function(cat) {
     var avgs = categoryAggregate[cat].avgs;
-    var sum = avgs.reduce(function(a, b) {return a + b});
-    aggregateOutput[cat].navg = avgs.length;
-    aggregateOutput[cat].avg = sum / aggregateOutput[cat].navg;
-    var medians = categoryAggregate[cat].medians; 
-    sum = medians.reduce(function(a, b) {return a + b});
-    aggregateOutput[cat].nmedian = medians.length;
-    aggregateOutput[cat].median = sum / aggregateOutput[cat].nmedian;
+    if (avgs.length > 0) {
+      var sum = avgs.reduce(function(a, b) {return a + b});
+      aggregateOutput[cat].navg = avgs.length;
+      aggregateOutput[cat].avg = sum / aggregateOutput[cat].navg;
+      var medians = categoryAggregate[cat].medians; 
+      sum = medians.reduce(function(a, b) {return a + b});
+      aggregateOutput[cat].nmedian = medians.length;
+      aggregateOutput[cat].median = sum / aggregateOutput[cat].nmedian;
+    } else {
+      aggregateOutput[cat].navg = null;
+      aggregateOutput[cat].avg = null;
+      aggregateOutput[cat].nmedian = null;
+      aggregateOutput[cat].median = null;
+    }
   });
   fs.writeFile('global.jsonp', "aggregateData=" + JSON.stringify(aggregateOutput, null, 2));
   fs.writeFile('whole.jsonp', "whole=" + JSON.stringify(whole, null, 2));
